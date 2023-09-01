32a7d355e569b5b9512636a8afae032d0de6316e
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-01 18:55:55 +0100

    home push

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -10,12 +10,17 @@ const CustomButton = ({ title, type, customStyles, handleClick }) => {
         backgroundColor: snap.color,
         color: '#fff',
       }
+    } else if (type === 'outline') {
+      return {
+        backgroundColor: 'black',
+        color: 'white',
+      }
     }
   }
 
   return (
     <button
-      className={`px-2 py-1.2 flex-1 rounded-md ${customStyles} bg-red-200`}
+      className={`px-2 py-2 flex-1 rounded-md ${customStyles} bg-red-200`}
       style={generateStyle(type)}
       onClick={handleClick}
     >
