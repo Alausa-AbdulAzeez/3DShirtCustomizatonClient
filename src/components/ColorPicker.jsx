1a1922fb5b231cc549127dc8a7f9a862fd61a6dc
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-09-07 16:09:03 +0100

    made color picker permanent

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,19 +1,21 @@
 import { SketchPicker } from 'react-color'
 import { useSnapshot } from 'valtio'
+import { motion } from 'framer-motion'
 
 import state from '../store'
+import { fadeAnimation } from '../config/motion'
 
 const ColorPicker = () => {
   const snap = useSnapshot(state)
 
   return (
-    <div className='absolute left-full ml-3'>
+    <motion.div className='absolute left-full ml-3' {...fadeAnimation}>
       <SketchPicker
         color={snap.color}
         disableAlpha
         onChange={(color) => (state.color = color.hex)}
       />
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -20,7 +20,7 @@ const CustomButton = ({ title, type, customStyles, handleClick }) => {
 
   return (
     <button
-      className={`px-2 py-2 flex-1 rounded-md ${customStyles} bg-red-200`}
+      className={`px-2 py-2 flex-1 rounded-md ${customStyles} bg-red-200 max-h-12`}
       style={generateStyle(type)}
       onClick={handleClick}
     >
